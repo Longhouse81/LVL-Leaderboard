4b67634b31c8184cb917789b014a3a2919701dde
Longhouse81 <bartmeeus@hotmail.com>
2021-01-04 17:58:57 +0100

    updated variable names and username to string

--- UpdateLeaderboard.go
+++ UpdateLeaderboard.go
@@ -3,19 +3,19 @@
 {{$amount:= 1}}{{/*the amount we increase the lvl with*/}}
 {{$maxEntry:= 10}}{{/*we set the max amount of leaderboard entries*/}}
 {{$member:= (getMember .User.ID)}}
-{{$userLevel:= (toInt (dbIncr $member.User.ID "lvl" $amount))}}{{/*increase the LVL of the user with the amount you want*/}}
+{{$uLVL:= (toInt (dbIncr $member.User.ID "uLVL" $amount))}}{{/*increase the LVL of the user with the amount you want*/}}
 
 {{/*update leaderboard if needed*/}}
 {{$lb:= ""}}
-{{$uEntry:= sdict "userid" $member.User.ID "username" (or $member.Nick $member.User.Username) "lvl" $userLevel}}
+{{$uEntry:= sdict "uID" $member.User.ID "uString" $member.User.String "uLVL" $uLVL}}
 {{if ($lb =sdict (or (dbGet 0 "LeaderBoard").Value sdict))}}{{/*load leaderboard data if availble*/}}
 	{{$newLB:= cslice}}
 	{{$lowest:= 0}}
-	{{if or (gt $userLevel $lb.lowest) (lt $lb.length $maxEntry)}}
+	{{if or (gt $uLVL $lb.lowest) (lt $lb.length $maxEntry)}}
 		{{$isSet:= 0}}
 		{{range $i, $e:= $lb.entries}}
-			{{- if and (gt $userLevel .lvl) (not $isSet)}}
-				{{- if eq .userid $member.User.ID}}
+			{{- if and (gt $mLVL .uLVL) (not $isSet)}}
+				{{- if eq .uID $member.User.ID}}
 					{{- $newLB = ($newLB.Append $uEntry)}}
 					{{- $isSet = 1}}
 				{{- else}}
@@ -26,23 +26,23 @@
 					{{- end}}
 					{{- $isSet = 1}}
 				{{- end}}
-			{{- else if and (not (eq .userid $member.User.ID)) (lt $i $maxEntry)}}
+			{{- else if and (not (eq .uID $member.User.ID)) (lt $i $maxEntry)}}
 				{{- $newLB = ($newLB.Append .)}}
 			{{- end}}
 			{{- if or (eq (sub (len $lb.entries) 1) $i) (eq (sub $maxEntry 1) $i)}}
 				{{- if and (not $isSet) (le (len $newLB) $maxEntry)}}
 					{{- $newLB = ($newLB.Append $uEntry)}}
 				{{- end}}
-				{{- $lowest = (index $newLB (sub (len $newLB) 1)).lvl}}
+				{{- $lowest = (index $newLB (sub (len $newLB) 1)).uLVL}}
 			{{- end -}}
 		{{else}}
 			{{$newLB = ($newLB.Append $uEntry)}}
-			{{$lowest = $userLevel}}
+			{{$lowest = $uLVL}}
 		{{end}}
 		{{$lb = sdict "length" (len $newLB) "lowest" $lowest "entries" $newLB}}
 	{{end}}
 {{else}}
-	{{$lb = sdict "length" 1 "lowest" $userLevel "entries" (cslice $uEntry)}}
+	{{$lb = sdict "length" 1 "lowest" $uLVL "entries" (cslice $uEntry)}}
 {{end}}
 {{if $lb}}
 	{{dbSet 0 "LeaderBoard" $lb}}
--- showLeaderboard.go
+++ showLeaderboard.go
@@ -1,7 +1,7 @@
 {{/*---script 2 generate the leaderboard---*/}}
 {{if ($lb:= sdict (or (dbGet 0 "LeaderBoard").Value sdict))}}{{/*load leaderboard data if availble*/}}
 {{range $i, $e:= $lb.entries}}{{/*show leaderboard*/}}
-{{(add $i 1)}}. **{{.username}}** LVL {{.lvl}}{{end}}
+{{(add $i 1)}}. **{{.uString}}** LVL {{.uLVL}}{{end}}
 {{else}}
 no entries yet
 {{end}}
